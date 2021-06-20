2b6320ec97d42010718a87d82b5e936117b2893a
lkqas <86144499+lkqas@users.noreply.github.com>
2021-06-20 18:02:10 +0300

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -3,10 +3,5 @@ pkg install python
 pkg install git
 git clone https://github.com/lkqas/god-bomber.git
 cd god-bomber/
-pip install termcolor
-pip install requests
+pip install -r requirements.txt
 python godbomber.py
-
-print("обновление прошло успешно, перезапустите программу.")
-
-sys.exit()
