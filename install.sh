e0ffe85a2774273b97947f13ea81fcb5b7bb8604
lkqas <86144499+lkqas@users.noreply.github.com>
2021-06-24 22:35:26 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-apt update
-apt upgrade
-apt install git python
-pip install requests termcolor
-git clone https://github.com/lkqas/god-bomber ~/.gb
-echo 'alias godbomber="python ~/.gb/godbomber.py"' >> ~/../usr/etc/bash.bashrc
-echo "GodBomber установлен!"
