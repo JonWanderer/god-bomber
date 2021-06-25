fdbff8e558b1ae6015ec311bfd8dda242fd98dab
lkqas <86144499+lkqas@users.noreply.github.com>
2021-06-25 20:18:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 apt update
-apt upgrade -y
-apt install -y git python
+apt upgrade
+apt install git python
 pip install requests termcolor
 git clone https://github.com/lkqas/god-bomber ~/.gb
 echo 'alias godbomber="python ~/.gb/godbomber.py"' >> ~/../usr/etc/bash.bashrc
 echo "GodBomber установлен!"
-echo "Для запуска бомбера пропишите - godbomber"
