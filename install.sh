3bdb220723a2a5c76a669ed160da4617b03b12b2
lkqas <86144499+lkqas@users.noreply.github.com>
2021-06-28 06:45:38 +0300

    up

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 apt update
 apt upgrade
 apt install -y git python
+pip install GitPython
 pip install requests termcolor GitPython
 git clone https://github.com/lkqas/god-bomber ~/.gb
 echo 'alias godbomber="python ~/.gb/godbomber.py"' >> ~/../usr/etc/bash.bashrc
