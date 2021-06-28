7b6509f7096eec17f06de735c1efe197ebd8dfa5
lkqas <86144499+lkqas@users.noreply.github.com>
2021-06-28 04:21:22 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,6 +1,18 @@
 import git
+from termcolor import colored
+import sys
+import requests
+
+def net_check():
+    try:
+        requests.get('https://www.google.com', verify=True)
+    except:
+        print(colored("Замечен плохой интернет....",'red'))
+        print(colored('Возобновите интернет, и перезайдите в бомбер...','red'))
+        sys.exit()
 
 def up():
+    net_check()
     repo = git.Repo("~/.gb")
     repo.remotes.origin.pull()
     print("Бомбер обновлен!")
